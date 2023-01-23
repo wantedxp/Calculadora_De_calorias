cfb25d626a9793515b179ed5f95ee2bf481a76d0
Filipe Campos <116318582+wantedxp@users.noreply.github.com>
2023-01-23 01:59:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+const form = document.getElementById('form');
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const gender = getSelectedValue('gender');
+    const age = getInputNumberValue('age');
+    const weight = getInputNumberValue('weight');
+    const height = getInputNumberValue('height');
+    const activityLevel = getSelectedValue('activity_level');
+
+    //Math.round para arendondar o valor das calorias para melhorar a visualização do usuário
+    const tmb = Math.round(
+
+
+        gender === 'female' // Aqui também poderia ter sido usado uma condicional if e else para tomada de desição mas decidi usar o operador ternário para ficar mais simplificado.
+            ? (655 + (9.6 * weight) + (1.8 * height) - (4.7 * age))
+            : (66 + (13.7 * weight) + (5 * height) - (6.8 * age))
+    );
+    const maintenance = Math.round(tmb * Number(activityLevel));
+    const loseWeight = maintenance - 450;
+    const gainWeight = maintenance + 450;
+
+    const layout = `
+    <h2>Aqui está o resultado:</h2>
+    <div class="result-content">
+      <ul>
+        <li>
+          Seu metabolismo basal é de <strong>${tmb} calorias</strong>.
+        </li>
+        <li>
+          Para manter o seu peso você precisa consumir em média <strong>${maintenance} calorias</strong>.
+        </li>
+        <li>
+          Para perder peso você precisa consumir em média <strong>${loseWeight} calorias</strong>.
+        </li>
+        <li>
+          Para ganhar peso você precisa consumir em média <strong>${gainWeight} calorias</strong>.
+        </li>
+      </ul>
+    </div>
+  `;
+
+    const result = document.getElementById('result');
+
+    result.innerHTML = layout;
+}
+
+function getSelectedValue(id) {
+    const select = document.getElementById(id);
+    return select.options[select.selectedIndex].value;
+}
+
+function getInputNumberValue(id) {
+    return Number(document.getElementById(id).value);
+}
